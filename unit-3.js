5b1a91fcecd99ab50d7681af4da0d492800032bc
Konstantin Ivanov <kelvin_91@mail.ru>
2022-02-14 14:29:28 +0300

    Conditional Statements and Data Types

---
+++ unit-3.js
@@ -0,0 +1,23 @@
+//addition of various types
+let s = true + " story";
+let p = 11 + "february";
+let a = 23 + false;
+console.log (s,p,a);
+//multiplication of various types
+let x = true * " 123";
+let y = '11' * 11;
+let z = 11 * true;
+console.log (x,y,z);
+//division of various types
+let b = '11' / false;
+let c = '11' / 2;
+let d = 11 / true;
+console.log (b,c,d);
+//explicit conversion
+let e = 11;
+e = String(e);
+console.log (typeof e)
+let f = '11'
+f = Number(f)
+console.log(typeof f)
+console.log (Boolean(11))
